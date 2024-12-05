e3c28db1ff0972137a426458409cdf22b0856e9a
Chris Marshall <thoughtpalettechris@gmail.com>
2024-12-04 19:40:07 -0600

    Can now query lists by url param on list page load

--- src/pages/lists/lists.component.ts
+++ src/pages/lists/lists.component.ts
@@ -2,29 +2,33 @@ import { Component, OnInit } from '@angular/core';
 import { HeaderNavComponent } from "../../shared/header-nav/header-nav.component";
 import { map, Observable, take } from 'rxjs';
 import { Apollo, gql } from 'apollo-angular';
+import { AsyncPipe, JsonPipe } from '@angular/common';
+import { ActivatedRoute } from '@angular/router';
 
 const GET_LISTS = gql`
-  query getWishLists {
-    lists
+  query getWishLists($id: String!) {
+    getLists(id: $id) {
+      id,
+      name,
+    }
   }
 `
 
 @Component({
   selector: 'lists',
   standalone: true,
-  imports: [HeaderNavComponent],
+  imports: [HeaderNavComponent, AsyncPipe, JsonPipe],
   templateUrl: './lists.component.html',
   styleUrl: './lists.component.scss'
 })
 export class ListsComponent implements OnInit {
   lists$!: Observable<any>
 
-  constructor(private apollo: Apollo) {}
+  constructor(private apollo: Apollo, private route: ActivatedRoute) {}
 
   ngOnInit(): void {
-    this.lists$ = this.apollo.query({ query: GET_LISTS}).pipe(map((result => {
-      console.log(result)
-      return result.data
-  })))
+    this.lists$ = this.apollo.query({ query: GET_LISTS, variables: {
+      id: this.route.snapshot.paramMap.get('id')
+    } })
   }
 }
