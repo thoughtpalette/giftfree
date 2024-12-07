3961482cfd3cce1fd1cc9c4683ebaa0fad3793f3
Chris Marshall <thoughtpalettechris@gmail.com>
2024-12-07 14:33:14 -0600

    Initial setup for adding a wishlist

--- src/pages/lists/lists.component.ts
+++ src/pages/lists/lists.component.ts
@@ -4,6 +4,8 @@ import { Observable } from 'rxjs';
 import { Apollo, gql } from 'apollo-angular';
 import { AsyncPipe, JsonPipe } from '@angular/common';
 import { ActivatedRoute, RouterLink } from '@angular/router';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { FormErrorComponent } from '../../shared/components/form-error/form-error.component';
 
 const GET_LISTS = gql`
   query getWishLists($id: String!) {
@@ -21,21 +23,74 @@ const GET_LISTS = gql`
   }
 `
 
+interface AddListRes {
+  addList: {
+    authorId: string
+  }
+}
+
+const CREATE_LIST = gql`
+  mutation createList($data: AddListInput!) {
+    addList(data: $data) {
+      authorId
+    }
+  }
+`
+
 @Component({
   selector: 'lists',
   standalone: true,
-  imports: [HeaderNavComponent, AsyncPipe, JsonPipe, RouterLink],
+  imports: [HeaderNavComponent, AsyncPipe, RouterLink, ReactiveFormsModule, FormErrorComponent],
   templateUrl: './lists.component.html',
   styleUrl: './lists.component.scss'
 })
 export class ListsComponent implements OnInit {
   lists$!: Observable<any>
+  form: FormGroup = new FormGroup({})
 
-  constructor(private apollo: Apollo, private route: ActivatedRoute) {}
+  constructor(
+      private apollo: Apollo,
+      private route: ActivatedRoute,
+      private fb: FormBuilder) 
+  {
+    this.form = this.fb.group({
+      name: ['', Validators.required],
+      eventDate: undefined,
+      description: ''
+    })
+  }
 
   ngOnInit(): void {
-    this.lists$ = this.apollo.query({ query: GET_LISTS, variables: {
+    this.lists$ = this.getLists()
+  }
+
+  private getLists() {
+    return this.apollo.query({ query: GET_LISTS, variables: {
       id: this.route.snapshot.paramMap.get('userId')
-    } })
+    }})
+  }
+
+  createList(modalRef: HTMLDialogElement) {
+    this.apollo.mutate({ mutation: CREATE_LIST, variables: {
+      data: {
+        ...this.form.value,
+        authorId: this.route.snapshot.paramMap.get('userId')
+      }
+    }}).subscribe((res) => {
+      console.log(res)
+      const id = (res.data as AddListRes).addList.authorId
+
+      // TODO: Should be a better way to handle this
+      this.lists$ = this.apollo.query({ 
+        query: GET_LISTS, 
+        variables: {
+          id: id
+        },
+        fetchPolicy: 'network-only',
+      })
+    })
+
+    this.form.reset()
+    modalRef.close()
   }
 }
