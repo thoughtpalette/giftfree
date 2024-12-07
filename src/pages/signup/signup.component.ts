10be1624d6692e08b253ecadbdfe6edf4fa68ced
Chris Marshall <thoughtpalettechris@gmail.com>
2024-12-07 13:14:02 -0600

    Updated signup form to FormGroup, added validation, fixed label ids

--- src/pages/signup/signup.component.ts
+++ src/pages/signup/signup.component.ts
@@ -1,29 +1,36 @@
 import { Component } from '@angular/core';
 import { UserService } from '../../services/user.service';
-import { FormsModule } from '@angular/forms';
+import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import { HeaderNavComponent } from '../../shared/header-nav/header-nav.component';
-
-interface SignupInput {
-  email: String
-  firstname: String
-  lastname: String
-  password: String
-}
+import { FormErrorComponent } from '../../shared/components/form-error/form-error.component';
+import { EMAIL_PATTERN, PASSWORD_PATTERN } from '../../shared/utils/input-patterns';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'signup',
   standalone: true,
-  imports: [HeaderNavComponent, FormsModule],
+  imports: [HeaderNavComponent, ReactiveFormsModule, FormErrorComponent, RouterLink],
   templateUrl: './signup.component.html',
   styleUrl: './signup.component.scss'
 })
 export class SignupComponent {
-  user: SignupInput = { firstname: '', lastname: '', email: '', password: ''}
+  form: FormGroup = new FormGroup({})
 
-  constructor(private userSvc: UserService) {
+  constructor(private userSvc: UserService, private fb: FormBuilder) {
+    this.form = this.fb.group({
+      firstname: ['', Validators.required],
+      lastname: ['', Validators.required],
+      email: ['', [Validators.required, Validators.pattern(EMAIL_PATTERN)]],
+      password: ['', [
+          Validators.required, 
+          Validators.minLength(8),
+          Validators.pattern(PASSWORD_PATTERN)
+        ]
+      ]
+    })
   }
 
   createUser() {
-    this.userSvc.create(this.user)
+    this.userSvc.create(this.form.value)
   }
 }
